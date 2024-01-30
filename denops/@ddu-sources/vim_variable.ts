32a8ebb861689b979c5166ab1473614825ac67e4
flow6852 <flow6852math@gmail.com>
2024-01-30 22:21:32 +0900

    Update

--- denops/@ddu-sources/vim_variable.ts
+++ denops/@ddu-sources/vim_variable.ts
@@ -3,9 +3,9 @@ import {
   Context,
   Item,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { Denops, fn, vars } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
-import { assert, is } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.1/types.ts";
+import { Denops, fn, vars } from "https://deno.land/x/ddu_vim@v3.10.1/deps.ts";
+import { assert, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 
 type Params = {
   bufnr: number;
